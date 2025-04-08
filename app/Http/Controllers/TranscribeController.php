bd495a42d950f9ccfbd509a4d3c98d7295c64ea9
Jahmil <roy_animo@hotmail.com>
2025-04-08 21:51:00 +0200

    preparing api-key and file upload CRUD

--- app/Http/Controllers/TranscribeController.php
+++ app/Http/Controllers/TranscribeController.php
@@ -8,8 +8,9 @@ class TranscribeController extends Controller
 {
     private $token;
 
-    public function __construct(){
-
+    public function __construct()
+    {
+        $this->token = config('services.openai.secret');
     }
 
     public function getView(){
@@ -17,6 +18,14 @@ class TranscribeController extends Controller
     }
 
     public function transcribe(){
-        
+        $audioInput = fopen(storage_path('path to file'),'r'); //file path
+
+        $docPath = stream_get_meta_data($audioInput)['uri']; 
+
+        $character = '/';
+
+        $trimPosition = strrpos($docPath, $character);
+
+        $pathTrim = substr($docPath,$trimPosition); //trim to only keep original file name
     }
 }
