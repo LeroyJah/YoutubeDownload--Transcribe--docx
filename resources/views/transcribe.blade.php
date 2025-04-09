5cd6395c1ddee187927e627900be57aec5421734
Jahmil <roy_animo@hotmail.com>
2025-04-09 23:09:55 +0200

    implementing file upload

--- resources/views/transcribe.blade.php
+++ resources/views/transcribe.blade.php
@@ -5,16 +5,22 @@
 <h1>
     Welcome to the Transcribe Page
 </h1>
-<div class="flex flex-row m-2 h-96">
-    <div class="w-1/6 border bg-white">Upload videos here:
-        <div>
-            {{"hello world"}}
+<div class="flex flex-row m-2 h-96 border rounded-lg">
+    <div class="w-1/6 bg-white flex flex-col justify-center border rounded-l-lg">
+        <div class="h-1/3 flex justify-center">
+            Upload videos here:
+        </div>
+        <div class="h-2/3 flex justify-center overflow-scroll">
+            <form action="" method="post" enctype="multipart/form-data">
+                <input type="file" name="fileUpload" id="fileUpload">
+                <x-file-upload.button type="submit">Upload</x-file-upload.button>
+            </form>
         </div>
     </div>
-    <div class="w-4/6 flex justify-center bg-white rounded p-2 border">
+    <div class="w-4/6 flex justify-center bg-white p-2 border">
         <x-file-upload.button>Whisper</x-file-upload.button>
     </div>
-    <div class="w-1/6 bg-white border">
+    <div class="w-1/6  flex justify-center bg-white border rounded-r-lg">
         Retrieve .docx here:
     </div>
 </div>
