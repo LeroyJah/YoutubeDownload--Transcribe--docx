c55ffbd61e58d6497e30fc77711bb40c84a38045
Jahmil <roy_animo@hotmail.com>
2025-04-13 23:39:33 +0200

    retrieving uploadedfile original name

--- app/Http/Controllers/fileUploadController.php
+++ app/Http/Controllers/fileUploadController.php
@@ -8,8 +8,18 @@ use Illuminate\Support\Facades\Storage;
 class fileUploadController extends Controller
 {
     public function saveFile(Request $request){
-        $request->validate([
+        if($request->validate([
             'uploadedFile' => 'required|file|mimes:mp3,mp4|max:20480'
-        ]);
+        ])){
+            // $uploadedFile = $request->all()['uploadedFile'];
+
+            // $path = $request->file('uploadedFile')->store('files');
+
+            $file = $request->file('uploadedFile');
+
+            $name = $file->getClientOriginalName();
+
+            return $name;
+        }
     }
 }
