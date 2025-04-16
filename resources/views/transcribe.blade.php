75ecdc2246781f975a05fbc9d2c79aaa572d7145
Jahmil <roy_animo@hotmail.com>
2025-04-16 22:58:37 +0200

    changed tailwind cdn to vite

--- resources/views/transcribe.blade.php
+++ resources/views/transcribe.blade.php
@@ -5,16 +5,15 @@
 <h1>
     Welcome to the Transcribe Page
 </h1>
-<div class="flex flex-row m-2 h-96 border rounded-lg">
-    <div class="w-1/6 bg-white flex flex-col justify-center border rounded-l-lg">
+<div class="flex flex-row m-2 h-96 border border-gray-200 rounded-lg">
+    <div class="w-1/6 bg-white flex flex-col justify-center border border-gray-200 rounded-l-lg">
         <div class="h-1/3 flex justify-center">
             Upload videos here:
         </div>
         <div class="h-2/3 overflow-scroll p-2">
             <form action="/savefile" method="post" enctype="multipart/form-data">
                 @csrf
-                Select File:
-                <input type="file" name="uploadedFile" id="fileUpload">
+                <input class="border border-gray-300 hover:bg-gray-300 p-1 rounded" type="file" name="uploadedFile" id="fileUpload">
                 <x-file-upload.button class="my-2" type="submit">Upload</x-file-upload.button>
             </form>
             <div>
@@ -35,10 +34,10 @@
             </div>
         </div>
     </div>
-    <div class="w-4/6 flex justify-center bg-white p-2 border">
+    <div class="w-4/6 flex justify-center bg-white p-2 border border-gray-200">
         <x-file-upload.button>Whisper</x-file-upload.button>
     </div>
-    <div class="w-1/6  flex justify-center bg-white border rounded-r-lg">
+    <div class="w-1/6  flex justify-center bg-white border border-gray-200 rounded-r-lg">
         Retrieve .docx here:
     </div>
 </div>
