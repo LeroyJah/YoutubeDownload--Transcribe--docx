a8646ff30c8f638a64287dd33cc9636353446144
Jahmil <roy_animo@hotmail.com>
2025-04-25 23:40:22 +0200

    attempting to add <svg> through PHP Class

--- app/Livewire/Loading.php
+++ app/Livewire/Loading.php
@@ -7,9 +7,21 @@ use Livewire\Component;
 class Loading extends Component
 {
     public $style = "bg-blue-300 text-white rounded px-2 hover:bg-blue-600 mr-2";
+    public $buttonText = "Whisper"; 
+    public $icon = "";
 
     public function changeStyle($style){
         $this->style = $style;
+        $this->changeText('Processing..');
+        $this->changeIcon();
+    }
+
+    public function changeText($text){
+        $this->buttonText = $text;
+    }
+
+    public function changeIcon(){
+        $this->icon = "hello again";
     }
 
     public function render()
--- resources/views/livewire/loading.blade.php
+++ resources/views/livewire/loading.blade.php
@@ -1,5 +1,7 @@
 <div>
-    <button class="{{ $style }}" wire:click.prevent="changeStyle('text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center')">Whisper</button>
+    <button class="{{ $style }}" wire:click.prevent="changeStyle('text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center')">
+    {{ $icon }}    
+    {{ $buttonText }}</button>
 </div>
 
 <!-- <div>
