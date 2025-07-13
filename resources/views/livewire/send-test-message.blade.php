e9b20242bf27f9012b4258bf4257c6fdba401e40
Jahmil <roy_animo@hotmail.com>
2025-07-13 21:42:27 +0200

    switched up the layout

--- resources/views/livewire/send-test-message.blade.php
+++ resources/views/livewire/send-test-message.blade.php
@@ -1,24 +1,43 @@
 <div class=" flex flex-col w-full">
-    <div class="p-1 flex">
-        <div>
-            <label for="templateSelector">Templates:</label>
-            <select name="templateSelector" wire:model.live="templateName">
-                <option disabled value="">Select a template</option>
-                <option value="hello_world">hello_world</option>
-                <option value="location_list">Location</option>
-                <option value="FAQ">FAQ</option>
-                <option value="speak_with_manager">Speak with manager</option>
-            </select>
+    <div>
+        <h1 class="m-1">Template Selector:</h1>
+        <div class="py-10 p-1 m-1 flex border rounded">
+            <div>
+                <label for="templateSelector">Templates:</label>
+                <select name="templateSelector" wire:model.live="templateName">
+                    <option disabled value="">Select a template</option>
+                    <option value="hello_world">hello_world</option>
+                    <option value="location_list">Location</option>
+                    <option value="FAQ">FAQ</option>
+                    <option value="speak_with_manager">Speak with manager</option>
+                </select>
+            </div>
+            <div>
+                <button class="border rounded bg-blue-400 hover:bg-blue-700 text-white px-1 mx-1" wire:click="setDefaultNumber">Default</button>
+            </div>
+            <div>
+                <form action="{{ route('templateMessage') }}" method="post">
+                    @csrf
+                    <input type="text" value="{{ $phoneNumber }}" name="phonenumber" class="border rounded" placeholder="+316" wire:model.live="phoneNumber">
+                    <input type="hidden" value="{{ $templateName }}" name="templatename">
+                    <button class="border rounded bg-blue-400 hover:bg-blue-700 text-white px-1">Send template message</button>
+                </form>
+                @error('phoneNumber')
+                <div class="text-red-500">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
         </div>
-        <div>
-            <button class="border rounded bg-blue-400 hover:bg-blue-700 text-white px-1 mx-1" wire:click="setDefaultNumber">Default</button>
-        </div>
-        <div>
-            <form action="{{ route('templateMessage') }}" method="post">
+    </div>
+    <div>
+        <h1 class="m-1">Service Message sender:</h1>
+        <div class="py-10 p-1 m-1 border rounded">
+            <form action="{{ route('serviceMessage') }}" method="post">
                 @csrf
                 <input type="text" value="{{ $phoneNumber }}" name="phonenumber" class="border rounded" placeholder="+316" wire:model.live="phoneNumber">
-                <input type="hidden" value="{{ $templateName }}" name="templatename">
-                <button class="border rounded bg-blue-400 hover:bg-blue-700 text-white px-1">Send test message</button>
+                <input type="text" value="{{ $serviceMessage }}" name="servicemessage" class="border rounded" placeholder="Enter text here" wire:model.live="serviceMessage">
+                <button class="border rounded bg-blue-400 hover:bg-blue-700 text-white px-1">Send service message</button>
             </form>
             @error('phoneNumber')
             <div class="text-red-500">
@@ -26,18 +45,5 @@
             </div>
             @enderror
         </div>
-    </div>
-    <div class="p-1">
-        <form action="{{ route('serviceMessage') }}" method="post">
-            @csrf
-            <input type="text" value="{{ $phoneNumber }}" name="phonenumber" class="border rounded" placeholder="+316" wire:model.live="phoneNumber">
-            <input type="text" value="{{ $serviceMessage }}" name="servicemessage" class="border rounded" wire:model.live="serviceMessage">
-            <button class="border rounded bg-blue-400 hover:bg-blue-700 text-white px-1">Send test message</button>
-        </form>
-        @error('phoneNumber')
-        <div class="text-red-500">
-            {{ $message }}
-        </div>
-        @enderror
     </div>
 </div>
--- resources/views/wa.blade.php
+++ resources/views/wa.blade.php
@@ -6,9 +6,11 @@
 </h1>
 <div class="flex flex-row m-2 h-[70vh] border border-gray-200 rounded-lg">
     <div class="w-1/6 bg-white flex flex-col justify-center border border-gray-200 rounded-l-lg p-2">
-        <div class="h-1/3 flex justify-center p-5">
+        <div class="h-1/6 flex justify-center p-5">
         </div>
-        <div class="h-2/3 overflow-scroll">
+        <div class="h-5/6 overflow-scroll">
+            <h1 class="font-bold">Note:</h1>
+            <h1>Template messages are the only type that can be sent outside of a 24h customer service window.</h1>
         </div>
     </div>
     <div class="w-4/6 flex justify-center bg-white p-2 border border-gray-200">
@@ -18,6 +20,29 @@
         <div class="h-1/6">
         </div>
         <div class="h-5/6">
+            <h1 class="font-bold">Prices:</h1>
+            <table class="border rounded-lg">
+                <tr>
+                    <th>Type</th>
+                    <th>Price</th>
+                </tr>
+                <tr>
+                    <td>Marketing</td>
+                    <td>€0.1323</td>
+                </tr>
+                <tr>
+                    <td>Utility</td>
+                    <td>€0.0414</td>
+                </tr>
+                <tr>
+                    <td>Authentication</td>
+                    <td>€0.0414</td>
+                </tr>
+                <tr>
+                    <td>Service</td>
+                    <td>Free</td>
+                </tr>
+            </table>
         </div>
     </div>
 </div>
